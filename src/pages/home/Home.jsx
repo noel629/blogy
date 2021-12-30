924f8bf25ede75bf3d1e723730604cbde59314d9
Noel <nj.crispin20@gmail.com>
2021-12-30 15:08:18 +0800

    okay22

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -12,7 +12,7 @@ export default function Home() {
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const res = await axios.get("/posts" + search);
+      const res = await axios.get(`${process.env.REACT_APP_API_URL}/posts + search`);
       setPosts(res.data);
     };
     fetchPosts();
